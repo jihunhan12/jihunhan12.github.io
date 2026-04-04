2c5f865cf6013ff39a89e4c8fcda3f2a26d904de
jihunhan12 <jhan41233@gmail.com>
2026-04-04 22:38:21 +0000

    Deploying to gh-pages from @ jihunhan12/jihunhan12.github.io@73b98e9653c4ee52b97044b3cd5028677f6ac529 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jihunhan12/KAIST-CUVIC-lab.github.io",
+    "data-repo": "jihunhan12/jihunhan12.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
