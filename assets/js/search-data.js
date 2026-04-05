800fcd42d47dad5470bf607d73f536c48b7c7584
jihunhan12 <jhan41233@gmail.com>
2026-04-05 00:27:27 +0000

    Deploying to gh-pages from @ jihunhan12/jihunhan12.github.io@814050cadc1ca8151fff778e7f3233660a50218d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,6 +51,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/contact/";
           },
+        },{id: "nav-news",
+          title: "news",
+          description: "News",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
