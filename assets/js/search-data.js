f509a3bc1bb9c7a60f7db0446ffa25179d02a134
jihunhan12 <jhan41233@gmail.com>
2026-04-05 00:11:29 +0000

    Deploying to gh-pages from @ jihunhan12/jihunhan12.github.io@33958e3a6b291eb13b60e1dbd774d86581067e2c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,9 +430,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-project-title",
+          title: 'Project Title',
+          description: "Project period and sponsors",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
