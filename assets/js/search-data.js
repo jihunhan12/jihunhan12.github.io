c84ddad00f5c19d20e6605c89fa82c900284f3f7
jihunhan12 <jhan41233@gmail.com>
2026-04-05 22:26:02 +0000

    Deploying to gh-pages from @ jihunhan12/jihunhan12.github.io@b1d56036d5a45cd1e68034232f9a90588f848c91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "Research overview and projects of the lab",
+          description: "Research Overview &amp; Projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
