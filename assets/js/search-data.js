9056b79fcf387f4d07c61bf4ffd34cac19b13ee9
jihunhan12 <jhan41233@gmail.com>
2026-04-05 00:28:36 +0000

    Deploying to gh-pages from @ jihunhan12/jihunhan12.github.io@107757dbcc3e85d7b2fb9e2d4c17906b9d483264 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,19 +37,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-join-us",
-          title: "Join Us",
-          description: "Join Us",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/join_us/";
-          },
         },{id: "nav-contact",
           title: "Contact",
           description: "Course materials, schedules, and resources for classes taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/contact/";
+          },
+        },{id: "nav-join-us",
+          title: "Join Us",
+          description: "Join Us",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/join_us/";
           },
         },{id: "nav-news",
           title: "news",
