1dbf1dc9c1a9e4aa91e5f311850013a8d4812a80
jihunhan12 <jhan41233@gmail.com>
2026-04-05 22:32:25 +0000

    Deploying to gh-pages from @ jihunhan12/jihunhan12.github.io@d9bc73f7186ba2ac08cd379dea269cff901fab14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "People",
-          description: "Members of the CUVIC lab",
+          description: "Lab Members",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
