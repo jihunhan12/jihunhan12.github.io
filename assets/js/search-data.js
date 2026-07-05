efc40b80bdd5c75e9e327dab12a2e1c762c31308
jihunhan12 <jhan41233@gmail.com>
2026-07-05 14:23:58 +0000

    Deploying to gh-pages from @ jihunhan12/jihunhan12.github.io@4c8362ff13b6298bfe49d91b88c9717a6b428000 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,12 +403,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-professor-jihun-han-officially-joined-kaist-as-of-today",
+      },{id: "news-professor-jihun-han-officially-joined-kaist-as-of-today",
           title: 'Professor Jihun Han officially joined KAIST as of today!!',
           description: "",
           section: "News",},{id: "projects-project-title",
